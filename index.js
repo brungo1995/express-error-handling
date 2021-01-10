33b51d1f099848c00e41de9198200f5410db3999
edvaldo <brungo1995@gmail.com>
2021-01-10 21:28:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { handleError, ErrorHandler } = require('./helpers/error')
+const app = express()
+
+app.use(express.json())
+const PORT = process.env.PORT || 3033
+
+app.get('/', (req, res) => {
+  return res.status(200).json('Hello world');
+})
+
+app.get('/error', (req, res) => {
+  throw new ErrorHandler(500, 'Internal server error');
+});
+
+
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
+
+app.listen(PORT, () => console.log(`server listening at port ${PORT}`))
